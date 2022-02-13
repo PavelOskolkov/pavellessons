f4c32a7127a81b2280217c3e7e5819793c25deb6
Pavel Oskolkov <98887080+PavelOskolkov@users.noreply.github.com>
2022-02-13 14:33:43 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,28 @@
+apples = 10
+var_2 = 10000
+print(id(apples))
+print(id(var_2))
+a = 'hello'
+t = 'Life'
+b = 20
+c = 6
+print(a+t)
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b**c)
+print(b//c)
+
+
+
+
+
+
+
+
+
+
+
+
+
